c0502df5ad3b1063125280317754839590ade519
BlackPalaces <vaname25@gmail.com>
2024-01-11 19:30:14 +0700

    editสามารถแก้ไขรูปภาพได้

--- MusicAlbumWeb/Controllers/MusicAlbumsController.cs
+++ MusicAlbumWeb/Controllers/MusicAlbumsController.cs
@@ -128,6 +128,20 @@ namespace MusicAlbumWeb
         {
             if (ModelState.IsValid)
             {
+                // Check if a file is uploaded
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+                        var path = Path.Combine(Server.MapPath("~/Content/images"), fileName);
+                        file.SaveAs(path);
+                        musicAlbum.Musicpic = fileName;
+                    }
+                }
+
                 db.Entry(musicAlbum).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
