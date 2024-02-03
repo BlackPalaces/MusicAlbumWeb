f3f4126ae3614afc32e271ca4c23bd0079f72972
BlackPalaces <vaname25@gmail.com>
2024-02-03 13:46:57 +0700

    ทำให้chartแสดงได้อีกครั้ง

--- MusicAlbumWeb/MyModel.Context.cs
+++ MusicAlbumWeb/MyModel.Context.cs
@@ -18,6 +18,7 @@ namespace MusicAlbumWeb
         public Entities()
             : base("name=Entities")
         {
+            Configuration.ProxyCreationEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
